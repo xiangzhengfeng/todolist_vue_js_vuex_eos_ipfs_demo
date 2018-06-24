e31d13d4ebdcb4d0111dac5ce8e2d328cb998a52
nebulasotc <39431767+nebulasotc@users.noreply.github.com>
2018-06-24 13:21:01 +0800

    Update buyConfig.js

--- js/buyConfig.js
+++ js/buyConfig.js
@@ -1,3 +1,3 @@
 var transactionfailure = [0,1,2,3,4,6,9,7,15,20,22,23,18,33,37,41];
-var transactionsuccess = [5,8,12,14,13,16,19,33];
+var transactionsuccess = [5,8,12,13,14,16,17,19,21,33,34,38,42,45];
 var transactionhide = [33,24,36,35,32];
